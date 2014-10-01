4c8d074ad29e60726227e553ef356aa92b1ecb3a
Sean Humeniuk <seanhumeniuk@gmail.com>
2014-09-30 20:15:01 -0400

    Send updated paint statuses to the server

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -9,10 +9,39 @@ myApp.controller('PaintsController', ['$scope', '$http', function($scope, $http)
         $scope.brands = data.brand;
         $scope.paint_ranges = data.paint_range;
         $scope.status_keys = data.status_key;
+
+        var range_brand_hash = {};
+        for (var i = 0; i < $scope.paint_ranges.length; i++) {
+            range_brand_hash[$scope.paint_ranges[i].id] = $scope.paint_ranges[i].brand_id;
+        }
+
+        for (var i = 0; i < $scope.paints.length; i++) {
+            var range_id = $scope.paints[i].range_id;
+            $scope.paints[i].brand_id = range_brand_hash[range_id]
+        }
+
         $scope.list_page = 0;
         $scope.paints_per_page = 20;
+
       }).
       error(function(data, status, headers, config) {
         $scope.paints = "ERROR";
       });
+
+    $scope.UpdatePaintStatus = function(paint, statusID) {
+        var old_status = paint.status;
+        paint.status = statusID;
+        var post_data = "paint_id=" + paint.id +
+                       "&status=" + paint.status;
+        $http({method: 'POST',
+               url: '/paint_statuses',
+                data: post_data,
+               headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).
+            success(function(data, status, headers, config) {
+            }).
+            error(function(data, status, headers, config) {
+                paint.status = old_status;
+                window.alert("Error Connecting to Server");
+            });
+    };
 }]);
